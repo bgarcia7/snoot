98db0858f02288138f2e44a24b93d93e1fad89c0
brandongarcia <thebgarcia@gmail.com>
2026-05-01 18:47:54 -0400

    updates tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -23,7 +23,6 @@ test -s "dist/github-pages/index.html"
 test -s "dist/github-pages/Snoot.zip"
 test -s "dist/github-pages/snoot-mark.png"
 test -s "assets/snoot-icon-template.png"
-test -s "data/favorite-build.json"
 
 "${SEARCH[@]}" 'URLByAppendingPathComponent:@"Snoot"' PocketDragonNative.m
 "${SEARCH[@]}" 'image.template = YES' PocketDragonNative.m
@@ -32,15 +31,12 @@ test -s "data/favorite-build.json"
 "${SEARCH[@]}" 'exportShareImage' PocketDragonNative.m
 "${SEARCH[@]}" 'copyShareImage' PocketDragonNative.m
 "${SEARCH[@]}" 'exportLandingSpritesToDirectory' PocketDragonNative.m
-"${SEARCH[@]}" 'bundledFavoriteBuildDictionary' PocketDragonNative.m
 "${SEARCH[@]}" 'stylePanelButton' PocketDragonNative.m
 "${SEARCH[@]}" 'recentApps' PocketDragonNative.m
 "${SEARCH[@]}" 'NSMaxY\(self.screenFrame\) \+ 12\.0' PocketDragonNative.m
 "${SEARCH[@]}" 'Snoot' landing/index.html
 "${SEARCH[@]}" '../dist/Snoot.zip' landing/index.html
 "${SEARCH[@]}" 'snoot-walker' landing/index.html
-"${SEARCH[@]}" 'FavoriteBuild.json' tools/package_snoot.sh
-"${SEARCH[@]}" 'data/favorite-build.json' README.md
 "${SEARCH[@]}" 'href="Snoot.zip"' dist/github-pages/index.html
 if "${SEARCH[@]}" '../dist/Snoot.zip' dist/github-pages/index.html; then
   echo "dist/github-pages/index.html should use local Snoot.zip link" >&2
